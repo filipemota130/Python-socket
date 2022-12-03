fce00752894964c40e21071bd0bf01313a4d82f1
WallaceLCS <wlcs@ic.ufal.br>
2022-12-03 13:58:15 -0300

    adição de threads e cliente contínuo

--- cliente/cliente.py
+++ cliente/cliente.py
@@ -1,45 +1,49 @@
-import os,socket
-
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
+import os,socket, threading
 ip_server = '192.168.0.3'
-client.connect((ip_server, 9000))
 
-opcao = str(input("Escolha se quer fazer download ou upload:\n[1] - Fazer downlaod de um arquivo do servidor;\n[2] - Fazer upload de um arquivo para o servidor ;\n[3] - Finalizar o programa.\nInforme sua escolha:"))
-client.send(opcao.encode())
+while True:
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect((ip_server, 9000))
 
-if (opcao == '1'):
-    filelist = client.recv(4096).decode() #Recebe a lista de arquivos do servidor e mostra
-    print("Lista de arquivos no servidor:\n", filelist)
+    opcao = str(input("Escolha se quer fazer download ou upload:\n[1] - Fazer download de um arquivo do servidor;\n[2] - Fazer upload de um arquivo para o servidor ;\n[3] - Finalizar o programa.\nInforme sua escolha:"))
+    client.send(opcao.encode())
+
+    if (opcao == '1'):
+        filelist = client.recv(4096).decode() #Recebe a lista de arquivos do servidor e mostra
+        print("Lista de arquivos no servidor:\n", filelist)
+        
+        namefile = str(input("insira o nome do arquivo: "))
+        client.send(namefile.encode()) #envia uma string com o nome do arquivo a fazer download
+
+        with open(namefile,'wb') as file:
+            while True:
+                data = client.recv(1000000)
+                if data == b'erro':
+                    print("arquivo inexistente")
+                    os.remove(namefile)
+                    exit()
+                if not data:
+                    break
+                file.write(data)
+            print(f"{namefile} recebido!")
+        client.close()
     
-    namefile = str(input("insira o nome do arquivo: "))
-    client.send(namefile.encode()) #envia uma string com o nome do arquivo a fazer download
+    elif (opcao == '2'):
+        print("Lista de arquivos no cliente:\n", os.listdir()) #Mostra os arquivos no cliente
+        
+        namefile2 = str(input("insira o nome do arquivo: "))
+        client.send(namefile2.encode()) #envia uma string com o nome do arquivo a fazer upload
 
-    with open(namefile,'wb') as file:
-        while True:
-            data = client.recv(1000000)
-            if data == b'erro':
-                print("arquivo inexistente")
-                os.remove(namefile)
-                exit()
-            if not data:
-                break
-            file.write(data)
-        print(f"{namefile} recebido!")
+        try: #Função que tava na parte de download do servidor p/ enviar o arquivo
+            with open(namefile2, 'rb') as file:
+                for line in file.readlines():
+                    client.send(line)
+        except FileNotFoundError as error:
+            print(error)
+            client.send("erro".encode())
+        client.close()
 
-elif (opcao == '2'):
-    print("Lista de arquivos no cliente:\n", os.listdir()) #Mostra os arquivos no cliente
-    
-    namefile2 = str(input("insira o nome do arquivo: "))
-    client.send(namefile2.encode()) #envia uma string com o nome do arquivo a fazer upload
-
-    try: #Função que tava na parte de download do servidor p/ enviar o arquivo
-        with open(namefile2, 'rb') as file:
-            for line in file.readlines():
-                client.send(line)
-    except FileNotFoundError as error:
-        print(error)
-        client.send("erro".encode())
-    client.close()
-
-    
+    elif (opcao == '3'):
+        client.close()
+        exit()
+        
--- servidor/servidor.py
+++ servidor/servidor.py
@@ -1,52 +1,66 @@
-import os, socket
-port= 9000
-servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-servidor.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
+import os, socket, threading
 ip = '192.168.0.3'
-servidor.bind((ip, port))
+port= 9000
 
-while True:
-    servidor.listen(2)
-    print("esperando conexão...")
 
-    connection, address = servidor.accept()
+def download(connection): #função download
+    filelist = os.listdir()
+    filelist = str(filelist) #Envia a lista de arquivos no servidor
+    connection.send(filelist.encode())
+        
+    namefile = connection.recv(1024).decode() #Recebe a string do nome a fazer download
+        
+    try:
+        with open(namefile, 'rb') as file:
+            for line in file.readlines():
+                connection.send(line)
+    except FileNotFoundError as error:
+        print(error)
+        connection.send("erro".encode())
+    connection.close()
 
-    if connection:
-        print("ocorreu uma conexão com o usuario", address)
-       
-    opcao = connection.recv(1024).decode() #Recebe a opção desejada do cliente
+def upload(connection):
+    namefile2 = connection.recv(1024).decode() #Recebe a string do nome a fazer upload
+        
+    with open(namefile2,'wb') as file:
+        while True:
+            data = connection.recv(1000000)
+            if data == b'erro':
+                print("arquivo inexistente")
+                os.remove(namefile2)
+                exit()
+            if not data:
+                break
+            file.write(data)
+        print(f"{namefile2} recebido!")
+
+
+def Main():
+    servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    servidor.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     
-    if (opcao == '1'):
-        filelist = os.listdir()
-        filelist = str(filelist) #Envia a lista de arquivos no servidor
-        connection.send(filelist.encode())
-        
-        namefile = connection.recv(1024).decode() #Recebe a string do nome a fazer download
-        
-        try:
-            with open(namefile, 'rb') as file:
-                for line in file.readlines():
-                    connection.send(line)
-        except FileNotFoundError as error:
-            print(error)
-            connection.send("erro".encode())
-        connection.close()
-    
-    elif (opcao == '2'):
-        namefile2 = connection.recv(1024).decode() #Recebe a string do nome a fazer upload
-        
-        with open(namefile2,'wb') as file:
-            while True:
-                data = connection.recv(1000000)
-                if data == b'erro':
-                    print("arquivo inexistente")
-                    os.remove(namefile2)
-                    exit()
-                if not data:
-                    break
-                file.write(data)
-            print(f"{namefile2} recebido!")
+    servidor.bind((ip, port))
 
-    elif (opcao == '3'):
-        connection.close()
+    while True:
+        servidor.listen(5)
+        print("esperando conexão...")
+
+        connection, address = servidor.accept()
+
+        if connection:
+            print("ocorreu uma conexão com o usuario", address)
+        
+        opcao = connection.recv(1024).decode() #Recebe a opção desejada do cliente
+        
+        if (opcao == '1'):
+            thread = threading.Thread(target=download, args=[connection])
+            thread.start()
+    
+        elif (opcao == '2'):
+            thread = threading.Thread(target=upload, args=[connection])
+            thread.start()
+        
+        elif (opcao == '3'):
+            connection.close()
+
+Main()
