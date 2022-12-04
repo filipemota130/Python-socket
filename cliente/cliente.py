bac1241cbc87253ff9d47361b0b06d2acb3786cc
Rafael <rafinha02061@outlook.com>
2022-12-04 17:51:45 -0300

    adicionar opção de escolher

--- cliente/cliente.py
+++ cliente/cliente.py
@@ -1,4 +1,4 @@
-import os,socket, threading
+import os,socket, threading, ast, pick
 ip_server = '192.168.0.3'
 
 while True:
@@ -10,9 +10,26 @@ while True:
 
     if (opcao == '1'):
         filelist = client.recv(4096).decode() #Recebe a lista de arquivos do servidor e mostra
-        print("Lista de arquivos no servidor:\n", filelist)
+        filelist = filelist.replace("'.git',", '')
+        filelist = ast.literal_eval(filelist)
         
-        namefile = str(input("insira o nome do arquivo: "))
+        print("Lista de arquivos no servidor:\n")
+        
+        #for i in range(len(filelist)):
+            #print(f'{i+1}: {filelist[i]}')
+        
+        options = filelist
+        option, index = pick.pick(options, "", indicator='=>', default_index=0)
+        print(option)
+                
+            
+        #id_arquivo = int(input("Insira o ID do arquivo: "))
+        #if id_arquivo > len(filelist):
+            #namefile = ''
+        #else :
+            #namefile = filelist[id_arquivo -1]
+        
+        namefile = option
         client.send(namefile.encode()) #envia uma string com o nome do arquivo a fazer download
 
         with open(namefile,'wb') as file:
@@ -29,9 +46,23 @@ while True:
         client.close()
     
     elif (opcao == '2'):
-        print("Lista de arquivos no cliente:\n", os.listdir()) #Mostra os arquivos no cliente
+        print("Lista de arquivos no cliente:") #Mostra os arquivos no cliente
+    
+        #for i in range(len(os.listdir())):
+            #print(f'{i+1}: {os.listdir()[i]}')
         
-        namefile2 = str(input("insira o nome do arquivo: "))
+                
+        #id_arquivo = int(input("Insira o ID do arquivo: "))
+        #if id_arquivo > len(os.listdir()):
+            #namefile2 = ''
+        #else :
+            #namefile2 = os.listdir()[id_arquivo -1]
+            
+        options2 = os.listdir()
+        option2, index = pick.pick(options2, "", indicator='=>', default_index=0)
+        print(option2)
+        
+        namefile2 = option2
         client.send(namefile2.encode()) #envia uma string com o nome do arquivo a fazer upload
 
         try: #Função que tava na parte de download do servidor p/ enviar o arquivo
