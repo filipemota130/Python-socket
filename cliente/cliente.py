04d232d03ff2fdfcf3653adf9f4150fbac643ca2
filipemota130 <fsmota@ic.ufal.br>
2022-12-04 23:51:11 -0300

    release: alterações na 'interface'

--- cliente/cliente.py
+++ cliente/cliente.py
@@ -1,49 +1,52 @@
-import os,socket, threading
-ip_server = '192.168.0.3'
+import os,socket
 
-while True:
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect((ip_server, 9000))
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    opcao = str(input("Escolha se quer fazer download ou upload:\n[1] - Fazer download de um arquivo do servidor;\n[2] - Fazer upload de um arquivo para o servidor ;\n[3] - Finalizar o programa.\nInforme sua escolha:"))
-    client.send(opcao.encode())
+ip_server = 'localhost'
+client.connect((ip_server, 9000))
 
-    if (opcao == '1'):
-        filelist = client.recv(4096).decode() #Recebe a lista de arquivos do servidor e mostra
-        print("Lista de arquivos no servidor:\n", filelist)
-        
-        namefile = str(input("insira o nome do arquivo: "))
-        client.send(namefile.encode()) #envia uma string com o nome do arquivo a fazer download
+opcao = str(input(">>> Transferência de Arquivos por TCP <<<\n[1] - Fazer download de um arquivo do servidor;\n[2] - Fazer upload de um arquivo para o servidor;\n[3] - Encerrar a conexão com o servidor.\nInforme sua escolha: "))
+client.send(opcao.encode())
 
-        with open(namefile,'wb') as file:
-            while True:
-                data = client.recv(1000000)
-                if data == b'erro':
-                    print("arquivo inexistente")
-                    os.remove(namefile)
-                    exit()
-                if not data:
-                    break
-                file.write(data)
-            print(f"{namefile} recebido!")
-        client.close()
+if (opcao == '1'):
+    filelist = client.recv(4096).decode() #Recebe a lista de arquivos do servidor e mostra.
+    print("Lista de arquivos no servidor:\n", filelist)
     
-    elif (opcao == '2'):
-        print("Lista de arquivos no cliente:\n", os.listdir()) #Mostra os arquivos no cliente
-        
-        namefile2 = str(input("insira o nome do arquivo: "))
-        client.send(namefile2.encode()) #envia uma string com o nome do arquivo a fazer upload
+    namefile = str(input("Insira o nome do arquivo desejado: "))
+    client.send(namefile.encode()) #Envia uma string com o nome do arquivo a fazer download.
 
-        try: #Função que tava na parte de download do servidor p/ enviar o arquivo
-            with open(namefile2, 'rb') as file:
-                for line in file.readlines():
-                    client.send(line)
-        except FileNotFoundError as error:
-            print(error)
-            client.send("erro".encode())
-        client.close()
+    nope = 0
+    with open(namefile,'wb') as file:
+        while True:
+            data = client.recv(1000000)
+            if data == b'erro':
 
-    elif (opcao == '3'):
-        client.close()
+                print("Arquivo inexistente.")
+                nope = 1
+                break
+            if not data:
+                break
+            file.write(data)
+        if nope == 0: print(f"Arquivo \"{namefile}\" recebido.")
+    if nope == 1:
+        os.remove(namefile)
+        print("Conexão com o servidor encerrada.")
         exit()
-        
+elif (opcao == '2'):
+    print("Lista de arquivos no cliente:\n", os.listdir()) #Mostra os arquivos no cliente.
+    
+    namefile2 = str(input("Insira o nome do arquivo: "))
+    client.send(namefile2.encode()) #Envia uma string com o nome do arquivo a fazer upload.
+
+    try: #Função que tava na parte de download do servidor p/ enviar o arquivo.
+        with open(namefile2, 'rb') as file:
+            for line in file.readlines():
+                client.send(line)
+            print(f"Arquivo \"{namefile2}\" enviado para o servidor.")
+    except FileNotFoundError as error:
+        print("Arquivo inexistente.")
+        client.send("erro".encode())
+    print("Conexão com o servidor encerrada.")
+    client.close()
+elif (opcao == '3'): print("Conexão com o servidor encerrada.")
+else: print("Input inválido. Conexão com o servidor encerrada.")
